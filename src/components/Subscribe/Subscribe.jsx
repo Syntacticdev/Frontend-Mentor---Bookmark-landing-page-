635ad494c58b393f85352f8ae363511e35385772
syntacticdev <dsyntacticdev@gmail.com>
2022-10-31 07:56:58 +0100

    initial commit

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,32 +1,30 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./style.css"
 import { FORM_SUBMIT_EMAIL } from "../../utils/env"
-const Subscribe = (e) => {
+const Subscribe = () => {
     const [error, setError] = useState(false)
+    const [email, setEmail] = useState("")
     const [successful, setSuccessful] = useState(false)
-    const emailRef = useRef(null)
-    const handleFormSubmit = async (e) => {
-        const re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2-8})?$/
-        const matchAnEmail = re.test(emailRef.current.value)
-        if (!matchAnEmail) {
-            return setError(true)
+    const re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2-8})?$/
+    const matchAnEmail = re.test(email)
 
-        }
-    }
     useEffect(() => {
+        if (email && !matchAnEmail) {
+            setError(true)
+        }
         setTimeout(() => {
             setError(false)
             setSuccessful(false)
         }, 3000)
-    }, [error, successful])
+    }, [error, successful, email])
     return (
         <div className='subscribe'>
             <h4>35,000+ already joined</h4>
             <span>Stay up-to-date with what we’re doing</span>
-            <form action={`https://formsubmit.co/${FORM_SUBMIT_EMAIL}`} method="POST" onSubmit={handleFormSubmit} className="subscribe__form">
+            <form action={`https://formsubmit.co/${FORM_SUBMIT_EMAIL}`} method="POST" className="subscribe__form">
                 <div className={`${error && 'error'} form-group`}>
                     <div className="input__wrapper">
-                        <input name="email" ref={emailRef} type="email" placeholder='Enter your email address' required />
+                        <input name="email" onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Enter your email address' required />
                         {error && <img className='error-icon' src="/images/icon-error.svg" alt="" />}
                     </div>
                     {error && <div className="error_message">Whoops, make sure it's an email</div>}
@@ -36,7 +34,7 @@ const Subscribe = (e) => {
                 <input type="hidden" name="_subject" value="New Mail Subscription" />
                 <input type="hidden" name="_template" value="table" />
                 <input type="hidden" name="_captcha" value="false" />
-                <button type='submit'>Contact Us</button>
+                <button className={`${email && !matchAnEmail && 'disabled'}`} disabled={email && !matchAnEmail} type='submit'>Contact Us</button>
             </form>
         </div>
     )
